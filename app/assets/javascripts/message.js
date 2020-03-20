9a3bf694b01190da6cf2ce6f870c54503b51dd8c
oyama1600 <61821335+oyama1600@users.noreply.github.com>
2020-03-20 15:19:10 +0900

    メッセージを送信したとき、メッセージ画面を最下部にスクロールするようにしました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,6 +53,9 @@ $('#new_message').on('submit', function(e){
     })
       .done(function(data){
         var html = buildHTML(data);
+        $('.messages').append(html);      
+        $('form')[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
   })
 });
