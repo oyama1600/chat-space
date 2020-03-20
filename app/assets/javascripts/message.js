01ff2f25a1dff222b6af9a6d6917b1f28dae93ed
oyama1600 <61821335+oyama1600@users.noreply.github.com>
2020-03-20 14:53:28 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,48 @@
-$(function(){
-  
-  $('#new_message').on('submit', function(e){
+$(function(){ 
+    function buildHTML(message){
+      if ( message.image ) {
+        var html =
+          `<div class="message">
+            <div class="message__upper-info">
+              <div class="message__upper-info__talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__text__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+          `<div class="message">
+            <div class="message__upper-info">
+              <div class="message__upper-info__talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__text__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -12,6 +51,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
-
+      .done(function(data){
+        var html = buildHTML(data);
+    })
+  })
 });
