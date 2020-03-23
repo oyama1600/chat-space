820d1a674d50b0a5b340350e46b8604d698ac9c2
oyama1600 <61821335+oyama1600@users.noreply.github.com>
2020-03-23 17:14:08 +0900

    取得した投稿データを表示出来るようにしました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){ 
+  
     function buildHTML(message){
       if ( message.image ) {
         var html =
-          `<div class="message">
+          `<div class="message" data-message-id=${message.id}>
             <div class="message__upper-info">
               <div class="message__upper-info__talker">
                 ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
         return html;
       } else {
         var html =
-          `<div class="message">
+          `<div class="message" data-message-id="${message.id}">
             <div class="message__upper-info">
               <div class="message__upper-info__talker">
                 ${message.user_name}
@@ -57,9 +58,41 @@ $('#new_message').on('submit', function(e){
         $('form')[0].reset();
         $('.submit-btn').prop('disabled', false);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      })
+      .fail(function(){
+        alert('メッセージ送信に失敗しました');
+      })
+      
+      return false;
     })
-    .fail(function(massege){
-      alert('メッセージを入力してください');
-    })
-  })
+    
+    var reloadMessages = function() {
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        var insertHTML = '';
+        
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+  
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
